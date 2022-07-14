b75f7f618be9eca7a4daac4b7a536d7374c5bcd8
FelipeGomes-ALU <felipecavalcanti321@gmail.com>
2022-07-14 00:28:33 -0300

    otimização do codigo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 from pygame.locals import *
 
 pygame.init()
@@ -9,9 +11,6 @@ altura = 680
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('star dust')
 relogio = pygame.time.Clock()
-
-x = 325
-y = 600
 
 # cores
 
@@ -28,15 +27,17 @@ mod_vel = 0
 
 colunas = 7
 linhas = 6
+bolax = random.randint(50,largura-50)
 fonte = pygame.font.SysFont('ArialBlack', 50)
 pygame.mixer.init()
 pygame.mixer.music.load('RUSH E editada.mp3')
 
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play()
+
 # funções do jogo
-fonte = pygame.font.SysFont('arial', 50)
-pygame.time.set_timer(pygame.USEREVENT, 5000)
+pygame.font.SysFont('arial', 50)
+pygame.time.set_timer(pygame.USEREVENT, 10000)
 som_impacto = pygame.mixer.Sound('som_impacto.wav')
 
 def fundodetela():
@@ -46,13 +47,13 @@ def fundodetela():
         bloco.desenhar(tela)
     plataforma_Jogador.desenhar_plataforma(tela)
 
-#mapa externo
+#  mapa externo
 
     pygame.draw.line(tela, preto, (0, largura), (0, 0), 10)
     pygame.draw.line(tela, preto, (0, 3), (720, 2), 10)
     pygame.draw.line(tela, preto, (largura, 1), (720, 680), 10)
     pygame.draw.line(tela, preto, (0, 680), (720, 680), 15)
-
+# gameover
     if gameover == 'true':
         if len(blocos) == 0:
             texto = fonte.render("parabens!, você venceu", 1, (255, 255, 255))
@@ -60,12 +61,10 @@ def fundodetela():
             texto = fonte.render("que pena, você perdeu!", 1, (255, 255, 255))
         tela.blit(texto, ((largura // 2 - texto.get_width() // 2), altura // 2 - texto.get_height() // 2))
         pygame.mixer.music.stop()
-        #playAgainText = font.render("Press Space to Play Again", 1, (255, 255, 255))
-        #win.blit(playAgainText, ((sw // 2 - playAgainText.get_width() // 2), sh // 2 + 30))
 
     pygame.display.update()
 
-
+# objetos e funções
 class plataforma(object):
     def __init__(self, x, y, l, a, cor):
         self.x = x
@@ -84,7 +83,7 @@ class Bola(object):
         self.y = y
         self.r = r
         self.cor = cor
-        self.vx = -3
+        self.vx = random.choice([ 3, -3])
         self.vy = -3
 
     def desenhar(self, tela):
@@ -123,7 +122,7 @@ def init():
 
 
 init()
-Bola = Bola(400, 550, 9, amarelo)
+Bola = Bola(bolax, 550, 9, amarelo)
 list_Bolas = [Bola]
 plataforma_Jogador = plataforma(largura / 2, altura - 75, 20, 150, (vermelho))
 
@@ -131,10 +130,9 @@ gameover = 'false'
 
 # core do jogo
 while 'TRUE':
-    relogio.tick(60)
+    relogio.tick(70)
     pygame.mouse.set_visible(bool(0))
     Bola.movimento()
-
 
 # controle da plataforma
     if pygame.mouse.get_pos()[0] - plataforma_Jogador.l // 2 < 0:
@@ -156,15 +154,12 @@ while 'TRUE':
     for bloco in blocos:
         if (Bola.x >= bloco.x and Bola.x <= bloco.x + bloco.l) or Bola.x + Bola.r >= bloco.x and Bola.x + Bola.r <= bloco.x + bloco.l:
             if (Bola.y >= bloco.y and Bola.y <= bloco.y + bloco.a) or Bola.y + Bola.r >= bloco.y and Bola.y + Bola.r <= bloco.y + bloco.a:
-                bloco.visible = False
+                #bloco.visible = False
                 blocos.pop(blocos.index(bloco))
                 Bola.vy *=-1
                 som_bloco = pygame.mixer.Sound('sombloco4.wav')
                 som_bloco.set_volume(0.5)
                 som_bloco.play()
-
-
-
 
  # fisica das paredes
     if Bola.x + Bola.r>= largura:
@@ -183,17 +178,10 @@ while 'TRUE':
         som_impacto.play()
 
 # sistema de aumento da velocidade
-   # if mod_vel == 1:
-       # for event in pygame.event.get():
-       #     if event.type == pygame.USEREVENT:
-       #         Bola.vy *= 1.2
-       #         mod_vel = 0
-    #elif mod_vel == 0:
     for event in pygame.event.get():
         if event.type == pygame.USEREVENT:
             Bola.vx *= 1.2
             Bola.vy *= 1.2
-             #mod_vel = 1
 # GameOver
     if Bola.y > 680 or len(blocos) == 0:
         gameover = 'true'
@@ -204,8 +192,3 @@ while 'TRUE':
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-
-
-
-
-
